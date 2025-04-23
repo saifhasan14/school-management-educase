09f0a69b3c62c876d0877aa8dcfcb854f91ffe70
saifhasan14 <saifhasan929@gmail.com>
2025-04-23 13:16:04 +0530

    added model and routes

---
+++ models/schoolModel.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const School = sequelize.define("School", 
+  {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  latitude: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  longitude: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  }
+);
+
+export default School;
